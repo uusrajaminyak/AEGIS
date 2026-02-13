027ac2c75fec166a74b5e4cf2b23d960c38bd7c9
uusrajaminyak <yustinushendisetyawan@gmail.com>
2026-02-13 22:00:43 +0700

    MessageBoxW hooking

--- AEGIS_agent/core/sensor.cpp
+++ AEGIS_agent/core/sensor.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <windows.h>
-#include <vector>
+#include "MinHook.h"
+
+typedef int (WINAPI* MESSAGEBOXW)(HWND, LPCWSTR, LPCWSTR, UINT);
+MESSAGEBOXW fpMessageBoxW = NULL;
+
+int WINAPI DetourMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType) {
+    std::cout << "Detoured MessageBoxW called!" << std::endl;
+    std::wcout << L"Text: " << lpText << std::endl;
+    std::wcout << L"Caption: " << lpCaption << std::endl;
+    std::cout << "Hijacking..." << std::endl;
+    return fpMessageBoxW(hWnd, L"This is a test",L"Hacked by AEGIS", uType);
+}
 
 extern "C" {
     __declspec(dllexport) void InitSensor() {
-        std::cout << "[*] C++ Sensor Initialized" << std::endl;
-
-        std::vector<int> dummy_pids = {1024, 2048, 4096};
-        std::cout << "[*] System scan initialized " << dummy_pids.size() << " PIDs found" << std::endl;
-
-        for(int pid : dummy_pids) {
-            std::cout << "[*] Scanning PID: " << pid << std::endl;
+        std::cout << "Initializing Sensor..." << std::endl;
+        if (MH_Initialize() != MH_OK) {
+            std::cout << "Failed to initialize MinHook." << std::endl;
+            return;
         }
-        
+
+        if (MH_CreateHook((LPVOID)&MessageBoxW, (LPVOID)&DetourMessageBoxW, reinterpret_cast<LPVOID*>(&fpMessageBoxW)) != MH_OK) {
+            std::cout << "Failed to create hook for MessageBoxW." << std::endl;
+            return;
+        }
+
+        if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
+            std::cout << "Failed to enable hook for MessageBoxW." << std::endl;
+            return;
+        }
+
+        std::cout << "Sensor initialized and hook applied." << std::endl;
+
+        MessageBoxW(NULL, L"Original Message", L"Test", MB_OK);
     }
 }
